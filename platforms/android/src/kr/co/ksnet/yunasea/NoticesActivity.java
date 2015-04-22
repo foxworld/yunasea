e128e40539272b8e3a1d2f9507c9ca55e1d7ed60
Peter LEE <foxworld@gmail.com>
2015-04-22 12:11:30 +0900

    공지사항추가

---
+++ platforms/android/src/kr/co/ksnet/yunasea/NoticesActivity.java
@@ -0,0 +1,15 @@
+package kr.co.ksnet.yunasea;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by PeterLEE on 2015-04-22.
+ */
+public class NoticesActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notices);
+    }
+}
